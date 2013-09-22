35f77382e51ef6e0712d02707a8ed7c429a3859e
Aditya Muttur <amuttur123@gmail.com>
2013-09-22 19:07:43 +0530

    Animations for the TextView and ImageView. [Finish #56799892]

--- Perpendicular/src/main/java/pazzled/game/home/HomeActivity.java
+++ Perpendicular/src/main/java/pazzled/game/home/HomeActivity.java
@@ -3,6 +3,10 @@ package pazzled.game.home;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HomeActivity extends Activity {
 
@@ -12,6 +16,19 @@ public class HomeActivity extends Activity {
         setContentView(R.layout.activity_home);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        ImageView homeLogo = (ImageView) findViewById(R.id.pazzled_logo);
+        Animation logoSlideUp = AnimationUtils.loadAnimation(this, R.anim.abc_slide_in_bottom);
+        homeLogo.startAnimation(logoSlideUp);
+
+        TextView pazzledText = (TextView) findViewById(R.id.pazzled);
+        TextView labsText = (TextView) findViewById(R.id.labs);
+        Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+        pazzledText.startAnimation(shake);
+        labsText.startAnimation(shake);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +36,5 @@ public class HomeActivity extends Activity {
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
-    
+
 }
